86aded2d13679cf457b625375450d4cede71e6ec
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2024-12-09 16:21:39 -0500

    001: Inefficient solution for day 3 part 2

--- projects/001-advent-of-code-2024/python/days/day_03.py
+++ projects/001-advent-of-code-2024/python/days/day_03.py
@@ -5,9 +5,9 @@ from numbers import Number
 from typing import Iterable, Iterator
 
 
-def extract_multiplications_from_memory(
+def extract_operations_from_memory(
     computer_memory: str,
-) -> Iterator[Iterable[int]]:
+) -> Iterator[re.Match]:
     """Extract all multiplication pairs from the corrupted memory file.
 
     Multiplication pairs are all that can be found exactly like "mul(X,Y)"
@@ -15,8 +15,12 @@ def extract_multiplications_from_memory(
 
     multiplication_instruction_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
 
-    for match in re.finditer(multiplication_instruction_pattern, computer_memory):
-        yield map(int, match.groups())
+    return re.finditer(multiplication_instruction_pattern, computer_memory)
+
+
+def get_multiplication_pair_from_operation(operation: re.Match) -> tuple[int, int]:
+    """Get the numbers that need to be multiplied from an extracted operation"""
+    return tuple(map(int, operation.groups()))
 
 
 def multiply(obj: Iterable[Number]) -> Number:
@@ -27,13 +31,37 @@ def multiply(obj: Iterable[Number]) -> Number:
 def challenge_1(computer_memory: str) -> int:
     """Get multiplications out of corrupted memory file"""
     return sum(
-        multiply(mul_pair)
-        for mul_pair in extract_multiplications_from_memory(computer_memory)
+        multiply(get_multiplication_pair_from_operation(operation))
+        for operation in extract_operations_from_memory(computer_memory)
     )
+
+
+def challenge_2(computer_memory: str) -> int:
+    """Get multiplications out of corrupted memory file, taking into account conditional
+    operations that enable or disable processing.
+    """
+
+    conditional_pattern = r"(don't\(\)|do\(\))"
+
+    split = re.split(conditional_pattern, computer_memory)
+
+    enabled = True
+    total = 0
+    for fragment in split:
+        if fragment == "do()":
+            enabled = True
+        elif fragment == "don't()":
+            enabled = False
+        elif enabled:
+            total += challenge_1(fragment)
+
+    return total
 
 
 def solve(computer_memory: str):
 
     result_1 = challenge_1(computer_memory)
 
-    return [f"The result is: {result_1}"]
+    result_2 = challenge_2(computer_memory)
+
+    return [f"The result is: {result_1}", f"Result with conditionals is: {result_2}"]

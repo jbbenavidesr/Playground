7fa2306689558b6c5cd709359142e9f3a1bd2250
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2024-12-11 12:47:15 -0500

    001: Solution of day 4 part 1

---
+++ projects/001-advent-of-code-2024/python/days/day_04.py
@@ -0,0 +1,83 @@
+from typing import Iterable
+
+WordSearch = Iterable[str]
+
+LOOKUP_WORD = "XMAS"
+
+DIRECTIONS = [
+    (1, 0),
+    (1, 1),
+    (0, 1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+]
+
+
+def extract_words(
+    starting_position: tuple[int, int], word_search: WordSearch, length: int
+) -> list[str]:
+    """Get all the words of length that can be extracted starting on given position
+
+    Parameters:
+    -----------
+    starting_position: the tuple with the postion for starting the word lookup
+    word_search: the word search where the words should be extracted from
+    length: the length of words that will be extracted
+    """
+
+    words = []
+
+    for direction in DIRECTIONS:
+        word = ""
+        current_position = starting_position
+        for _ in range(length):
+            if any(pos < 0 for pos in current_position):
+                break
+
+            try:
+                word += word_search[current_position[1]][current_position[0]]
+            except IndexError:
+                break
+
+            current_position = (
+                current_position[0] + direction[0],
+                current_position[1] + direction[1],
+            )
+
+        if len(word) == length:
+            words.append(word)
+
+    return words
+
+
+def challenge_1(word_search: WordSearch) -> int:
+    """Count the number of times the word shows up in the word search"""
+
+    count = 0
+
+    for y, line in enumerate(word_search):
+        for x in range(len(line)):
+            words = extract_words(
+                starting_position=(x, y),
+                word_search=word_search,
+                length=len(LOOKUP_WORD),
+            )
+
+            count += words.count(LOOKUP_WORD)
+
+    return count
+
+
+def parse_input(input_string: str) -> WordSearch:
+    """Turn the input file into a 2D array of letters"""
+    return input_string.split("\n")
+
+
+def solve(word_search: WordSearch) -> list[str]:
+
+    result_1 = challenge_1(word_search)
+
+    return [f"The word {LOOKUP_WORD} show's up {result_1} times."]
